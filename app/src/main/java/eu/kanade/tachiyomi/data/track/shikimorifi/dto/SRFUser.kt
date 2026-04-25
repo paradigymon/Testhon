00a35997ef3f5547a7dfc8099d342b93159db598
paradigymon <paradigymon@gmail.com>
2026-04-25 09:30:59 +0300

    Create SRFUser.kt

---
+++ app/src/main/java/eu/kanade/tachiyomi/data/track/shikimorifi/dto/SRFUser.kt
@@ -0,0 +1,17 @@
+package eu.kanade.tachiyomi.data.track.shikimorifi.dto
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class SRFUser(
+    val id: Int,
+)
+
+@Serializable
+data class SRFAddMangaResponse(
+    val id: Long,
+)
+```
+
+### Шаг 3.23
+Нажми **"Commit changes"** → **"Commit changes"** ✅
